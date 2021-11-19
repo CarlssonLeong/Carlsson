6ccbc4f9848eade296b828bbcc73358f4f7962a0
CarlssonLeong <94687318+CarlssonLeong@users.noreply.github.com>
2021-11-19 19:33:13 +0800

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int MAX_LEN = 100;
+	char name[MAX_LEN] = {0};
+	cout << "Let me teach you how to introduce yourself :)" << "\n";
+	cout << "Please enter your name: " ; 
+	cin >> name;
+	cout << "My name is Macus Huong.";
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
